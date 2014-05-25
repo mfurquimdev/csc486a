272a6a44b09d18d538e7341bce44cb434c1aa0ad
Nicolas Guillemot <nlguillemot@gmail.com>
2014-05-25 00:09:49 -0700

    added initial ray/plane stuff

---
+++ include/ng/framework/geometry.hpp
@@ -0,0 +1,71 @@
+#ifndef NG_GEOMETRY_HPP
+#define NG_GEOMETRY_HPP
+
+#include "ng/engine/linearalgebra.hpp"
+
+#include <limits>
+
+namespace ng
+{
+
+template<class T>
+struct Ray
+{
+    // Ray equation: p = Origin + Direction * t
+
+    genType_base<T,3> Origin;
+    genType_base<T,3> Direction;
+
+    Ray() = default;
+
+    Ray(genType_base<T,3> origin, genType_base<T,3> direction)
+        : Origin(origin), Direction(direction)
+    { }
+};
+
+template<class T>
+struct Plane
+{
+    // Plane equation: Normal.x * x + Normal.y * y + Normal.z * z + d = 0
+    genType_base<T,3> Normal;
+    T D;
+
+    Plane() = default;
+
+    Plane(genType_base<T,3> normal, T d)
+        : Normal(normal)
+        , D(d)
+    { }
+
+    Plane(genType_base<T,3> normal, genType_base<T,3> pointOnPlane)
+        : Normal(normal)
+        , D(-(dot(normal, pointOnPlane)))
+    { }
+};
+
+template<class T>
+bool RayPlaneIntersect(const Ray<T>& ray, const Plane<T>& plane, T tmin, T tmax, T& t)
+{
+    T denom = dot(plane.Normal, ray.Direction);
+
+    if (std::abs(denom) <= std::numeric_limits<T>::epsilon())
+    {
+        return false;
+    }
+
+    T result = - (dot(plane.Normal, ray.Origin) + plane.D) / denom;
+
+    if (result >= tmin && result <= tmax)
+    {
+        t = result;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+} // end namespace ng
+
+#endif // NG_GEOMETRY_HPP
